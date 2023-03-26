c91bc498d1f171fa3c063f43a21ff48dd4e2b06e
muhamadherwan <hello.wanlab@gmail.com>
2023-03-26 11:18:26 +0800

    update categories row

---
+++ app/Http/Livewire/QuotationForm.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use App\Models\Category;
+use Livewire\Component;
+
+class QuotationForm extends Component
+{
+    public $categoryFirst;
+    public $categories = [];
+    public $count = 1;
+    public $successMessage;
+
+    public function rules()
+    {
+        return [
+            'categoryFirst' => 'required|min:3|max:255',
+            'categories.*' => 'required|min:3|max:255',
+        ];
+    }
+
+    public function messages()
+    {
+        return [
+            'categories.*.required' => 'This Category name is required.',
+            'categories.*.min' => 'This Category name must be at least 3 characters',
+            'categoryFirst.required' => 'This Category name is required.',
+            'categoryFirst.min' => 'This Category name must be at least 3 characters',
+        ];
+    }
+
+    public function addNameField()
+    {
+        $this->count = count($this->categories) + 1;
+        array_push($this->categories, '');
+    }
+
+    public function removeNameField($index)
+    {
+        unset($this->categories[$index]);
+        $this->count--;
+        $this->resetErrorBag('names.' . $index);
+    }
+
+    public function submit()
+    {
+        $this->validate();
+
+        $hasInput = false;
+
+        if (!empty(trim($this->categoryFirst))) {
+            $hasInput = true;
+            Category::create(['name' => $this->categoryFirst]);
+        }
+
+        foreach ($this->categories as $category) {
+            if (!empty(trim($category))) {
+                $hasInput = true;
+                Category::create(['name' => $category]);
+            }
+        }
+
+        $this->categories = [];
+        $this->count = 1;
+
+//        session()->flash('message', 'Name successfully saved.');
+//        $this->successMessage = 'data is saved!';
+
+        if ($hasInput) {
+            $this->successMessage = 'Data is saved!';
+        }
+
+        // clear the form data only if there are no errors
+        if (!$this->getErrorBag()->hasAny()) {
+            $this->categoryFirst = '';
+            $this->categories = [];
+            $this->count = 1;
+        }
+    }
+    
+    public function render()
+    {
+        return view('livewire.quotation-form');
+    }
+}
+
+
